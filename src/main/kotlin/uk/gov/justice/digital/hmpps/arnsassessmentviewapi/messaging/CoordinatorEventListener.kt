4eefffbe375cd4c98dba4ea1f99276551041576d
Ben H <ben.howe@hippodigital.co.uk>
2026-06-08 15:54:42 +0100

    Arn 3170 1 sns consumer plumbing (#21)
    * ARN-3170: Plumbing for consuming messages
    
    * ARN-3170: Adding missed files

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentviewapi/messaging/CoordinatorEventListener.kt
@@ -0,0 +1,24 @@
+package uk.gov.justice.digital.hmpps.arnsassessmentviewapi.messaging
+
+import io.awspring.cloud.sqs.annotation.SqsListener
+import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.stereotype.Component
+
+/**
+ * Consumes events from the coordinator SQS queue. Enabled by
+ * `app.coordinator-event-listener.enabled` so it stays opt-in per environment.
+ */
+@Component
+@ConditionalOnProperty("app.coordinator-event-listener.enabled", havingValue = "true")
+class CoordinatorEventListener {
+
+  @SqsListener("coordinator", factory = "hmppsQueueContainerFactoryProxy")
+  fun onMessage(rawMessage: String) {
+    log.info("Received message from coordinator queue: {}", rawMessage)
+  }
+
+  private companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+  }
+}
